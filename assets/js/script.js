a7e4115e1b96ca59c08b587f0382cd69982b708d
Bhuvan H R <101463687+Bhuvan2303@users.noreply.github.com>
2026-08-02 14:52:19 +0530

    Add active class toggle for navigation links

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,16 @@ topBtn.onclick = function () {
     });
 
 };
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", function(){
+
+        navLinks.forEach(item => item.classList.remove("active"));
+
+        this.classList.add("active");
+
+    });
+
+});
